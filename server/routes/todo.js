65926897215a12eb84d188c21d02fcfbc4fb6654
MBD_Ass1 <chairus06@gmail.com>
2017-04-29 20:40:46 +0930

    Add GET /todos route

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -2,6 +2,7 @@ var router = require("express").Router();
 var { Todo } = require("../models/todo");
 var { User } = require("../models/user");
 
+// Create - add a todo in the list
 router.post("/todos", (req, res) => {
     Todo.create({
         text: req.body.text
@@ -9,6 +10,16 @@ router.post("/todos", (req, res) => {
         res.send(todo);
     }, (err) => {
         res.status(400).send(err);
+    });
+});
+
+// Index - show all todos
+router.get("/todos", (req, res) => {
+    Todo.find().then((todos) => {
+        var todoList = todos.filter((todo) => !todo.completed)
+        res.send({todoList});
+    }, (err) => {
+        res.status(500).send(err);
     });
 });
 
