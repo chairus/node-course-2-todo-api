be43e4c8b12c038be54d0339bf7f5997a0001477
MBD_Ass1 <chairus06@gmail.com>
2017-05-03 12:32:34 +0930

    Setup database credentials

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 // Tell mongoose to use the built-in promise in javascript
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
-//mongoose.connect("mongodb://cyrus:Mfdtda-06@ds129651.mlab.com:29651/todos_app");
+//mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://cyrus:Mfdtda-06@ds129651.mlab.com:29651/todos_app");
 
 module.exports = { mongoose };
