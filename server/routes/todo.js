2c271df9498871523d209db40004b1f880aa6f4c
MBD_Ass1 <chairus06@gmail.com>
2017-05-03 10:41:42 +0930

    Add GET /todos/:id route

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,6 +1,7 @@
-var router = require("express").Router();
-var { Todo } = require("../models/todo");
-var { User } = require("../models/user");
+const router = require("express").Router();
+const { Todo } = require("../models/todo");
+const { User } = require("../models/user");
+const { ObjectID } = require("mongodb");
 
 // Create - add a todo in the list
 router.post("/todos", (req, res) => {
@@ -21,6 +22,25 @@ router.get("/todos", (req, res) => {
     }, (err) => {
         res.status(500).send(err);
     });
+});
+
+router.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    
+    // Validate ID
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    
+    // Find the todo with the associated ID
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(e => {
+        res.status(400).send();
+    })
 });
 
 module.exports = router;
