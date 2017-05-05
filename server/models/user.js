69aa2ad64bf7ba39a8e3bf8a699bb113636894aa
MBD_Ass1 <chairus06@gmail.com>
2017-05-05 17:21:52 +0930

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,12 @@ userSchema.methods.generateAuthToken = function () {
     });
 };
 
+userSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({ $pull: { tokens: { token } } });
+};
+
 userSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
--- server/routes/user.js
+++ server/routes/user.js
@@ -28,4 +28,17 @@ router.post("/users/login", (req, res) => {
     }).catch(err => res.status(400).send());
 });
 
+router.delete("/users/me/token", middleware.userAuthentication, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+
+    /* ALTERNATIVE SOLUTION */
+    // User.findByIdAndUpdate(req.user._id, { $pull: { tokens: {access:'auth', token:req.token} } }, { new: true }).then((user) => {
+    //     res.send(user);
+    // }).catch(err => res.status(500).send());
+});
+
 module.exports = router;
